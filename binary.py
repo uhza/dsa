8487ad773a3d0a4b02b1fb77dd51d8e11ede833d
George Itty <georgeitty@Georges-Air.domain.name>
2023-12-26 10:19:29 +0530

    initial commit

---
+++ binary.py
@@ -0,0 +1,12 @@
+
+def search(nums, target: int) -> int:
+    low, high = 0, len(nums)-1
+    while low <= high:
+        mid = (low+high)//2
+        if target == nums[mid]:
+            return mid
+        elif target < nums[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
